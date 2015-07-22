f1b2289291b507b528ebde72f4070e331229bec5
Adolfo R. Brandes <arbrandes@arbrand.es>
2015-07-22 18:04:41 -0300

    Use StudioEditableXBlockMixin
    This gives us free editing and saving of certain XBlock fields in the
    studio view.
    
    Also fix field definition indentation, and remove some fields that won't
    be used.

--- viaduct/viaduct.py
+++ viaduct/viaduct.py
@@ -2,60 +2,56 @@ from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String
 from xblock.fragment import Fragment
 from xblockutils.resources import ResourceLoader
+from xblockutils.studio_editable import StudioEditableXBlockMixin
 
 loader = ResourceLoader(__name__)
 
-class ViaductXBlock(XBlock):
+
+class ViaductXBlock(StudioEditableXBlockMixin, XBlock):
     """
     Provides lab environments for Viaduct classes.
     """
 
     # Scope: content
     gateone_url = String(
-            default="https://127.0.0.1",
-            scope=Scope.content,
-            help="Where the gateone server is running")
-
+        default="https://127.0.0.1",
+        scope=Scope.content,
+        help="Where the gateone server is running")
     os_heat_template = String(
-            default=None,
-            scope=Scope.content,
-            help="The OpenStack Heat template")
+        multiline_editor=True,
+        resettable_editor=False,
+        default="",
+        scope=Scope.content,
+        help="The OpenStack Heat template")
 
     # Scope: settings
     os_auth_url = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack authentication URL")
+        default="",
+        scope=Scope.settings,
+        help="The OpenStack authentication URL")
     os_tenant_name = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack tenant name")
+        default="",
+        scope=Scope.settings,
+        help="The OpenStack tenant name")
     os_user_name = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack user name")
+        default="",
+        scope=Scope.settings,
+        help="The OpenStack user name")
     os_password = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack password")
-    os_heat_flavor = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack Heat flavor")
-    os_heat_public_net_id = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack Heat public net ID")
-    os_heat_image = String(
-            default=None,
-            scope=Scope.settings,
-            help="The OpenStack Heat image")
+        default="",
+        scope=Scope.settings,
+        help="The OpenStack password")
 
     # Scope: user state
-    public_ip = String(
-            default=None,
-            scope=Scope.user_state,
-            help="The user environment's public IP")
+
+    editable_fields = (
+        'gateone_url',
+        'os_heat_template',
+        'os_auth_url',
+        'os_tenant_name',
+        'os_user_name',
+        'os_password',
+        'os_password')
 
     def student_view(self, context=None):
         """
