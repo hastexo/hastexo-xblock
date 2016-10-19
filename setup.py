035accaf91a30eb2236b004937cf1527a3eaba33
Adolfo R. Brandes <adolfo@hastexo.com>
2016-10-19 08:30:01 -0200

    Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def package_data(pkg, roots):
 
 setup(
     name='hastexo-xblock',
-    version='0.2.2',
+    version='0.2.3',
     description='hastexo XBlock',
     packages=[
         'hastexo',
