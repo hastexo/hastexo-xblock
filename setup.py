f0798e946356bc0bc474e8071f6b35124908b1f3
Adolfo R. Brandes <adolfo@hastexo.com>
2018-10-14 16:33:07 -0300

    Bump to v2.3.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_data(pkg, roots):
 
 setup(
     name='hastexo-xblock',
-    version='2.3.3',
+    version='2.3.4',
     description='hastexo XBlock: '
                 'Makes arbitrarily complex lab environments '
                 'available on an Open edX LMS',
