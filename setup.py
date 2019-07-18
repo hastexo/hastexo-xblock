7d453f15b9be9a5c10699af94597b8f2cd24c508
Adolfo R. Brandes <adolfo.brandes@citynetwork.eu>
2019-07-18 13:17:29 -0300

    Bump os-client-config

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     install_requires=[
         'XBlock',
         'xblock-utils',
-        'os-client-config==1.13.1',
+        'os-client-config==1.28.0',
         'osc-lib==1.2.0',
         'cliff==2.2.0',
         'oslo.utils==3.37.1',
